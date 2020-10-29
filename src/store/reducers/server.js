5d731ffd1be47fe34c45dd12559be242c6f58a29
Michael Shuff <mjshuff23@gmail.com>
2020-10-29 18:56:34 -0400

    updated servers in store

--- src/store/reducers/server.js
+++ src/store/reducers/server.js
@@ -1,12 +1,12 @@
 import { ADD_SERVER, ADD_SERVERS } from "../actions/server";
 
-export default function reducer(state = {}, action) {
+export default function reducer(state = [], action) {
     switch (action.type) {
         case ADD_SERVER: {
-            return {
-                ...state,
-            [action.serverId]: { title: action.title, id: action.serverId }
-            }
+            return [...state, {
+                title: action.title,
+                id: action.serverId,
+            }]
         }
 
         default:
