6f9a44f54a142e82bd1d54fe963ca44f65f7a982
Michael Shuff <mjshuff23@gmail.com>
2020-10-30 09:58:09 -0400

    minor fixes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,53 +7,47 @@ import ChannelList from './ChannelList';
 import './stylesheets/Sidebar.css'
 
 function Sidebar() {
-
     const [showChannel, setShowChannel] = useState(false);
-
+    // const [currentServer, setCurrentServer] = useState();
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getServers());
     }, [dispatch])
 
-    const showChannels = (event) => {
+    const showChannels = async (event) => {
         event.preventDefault();
         event.stopPropagation();
         setShowChannel(!showChannel);
+        console.log(event.target.id);
     }
 
     const servers = useSelector((state) => Object.values(state.server));
 
-        return (
-            <div className="sidebar">
-                {servers.map((server, index) => {
-                    let imgurl;
-                    const imgProps = {
-                        id: server.id,
-                    }
-                    if (index === 2) {
-                        imgurl = `http://www.mikeshuff.com/images/server${index + 1}.png`;
-
-                    } else {
-                        imgurl = `http://www.mikeshuff.com/images/server${index + 1}.jpg`;
-                    }
-                    return (
-                        <Tooltip key={server.id} title={server.title} placement='right'>
-                            <Avatar
-                                key={server.id}
-                                id={server.id}
-                                alt={server.title}
-                                onClick={showChannels}
-                                component='span'
-                                className='sidebar__serverBtn'
-                                imgProps={imgProps}
-                                src={imgurl}
-                            />
-                        </Tooltip>);
-                })}
-                {showChannel ? <ChannelList /> : null}
-            </div>
-        )
+    return (
+        <div className="sidebar">
+            {servers.map((server, index) => {
+                let imgurl;
+                const imgProps = { id: server.id, }
+                if (index === 2) { imgurl = `http://www.mikeshuff.com/images/server${index + 1}.png`; }
+                else { imgurl = `http://www.mikeshuff.com/images/server${index + 1}.jpg`; }
+                return (
+                    <Tooltip key={server.id} title={server.title} placement='right'>
+                        <Avatar
+                            key={server.id}
+                            id={server.id}
+                            alt={server.title}
+                            onClick={showChannels}
+                            component='span'
+                            className='sidebar__serverBtn'
+                            imgProps={imgProps}
+                            src={imgurl}
+                        />
+                    </Tooltip>);
+            })}
+            {showChannel ? <ChannelList /> : null}
+        </div>
+    )
 }
 
 export default Sidebar
