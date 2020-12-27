6595f7eb346448892579578f15fb0d6c664bfff2
Michael Shuff <mjshuff23@gmail.com>
2020-12-26 21:16:35 -0500

    Added logout button to chat header

--- src/components/AddServer.js
+++ src/components/AddServer.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { logout } from '../store/actions/authentication';
 import { createServer } from '../store/actions/server';
 import './stylesheets/AddServer.css';
 
@@ -9,33 +8,24 @@ function AddServer({ socket }) {
   const dispatch = useDispatch();
   const [serverTitle, setServerTitle] = useState('');
 
-
-  const handleLogout = (event) => {
-    event.preventDefault();
-    dispatch(logout())
-  }
-
   const handleServerCreate = (event) => {
     event.preventDefault();
-    dispatch(createServer(serverTitle, socket))
-  }
+    dispatch(createServer(serverTitle, socket));
+  };
 
   const updateServerTitle = (event) => {
-    setServerTitle(event.target.value)
-  }
+    setServerTitle(event.target.value);
+  };
 
-return (
+  return (
     <div className="serverForm">
       <h1>Create A Server</h1>
-      <form onSubmit={handleServerCreate}>
-        <input onChange={updateServerTitle} value={serverTitle}  placeholder="Server Title" type="text"/>
+      <form onSubmit={ handleServerCreate }>
+        <input onChange={ updateServerTitle } value={ serverTitle } placeholder="Server Title" type="text" />
         <button type="submit">Create Server</button>
       </form>
-
-      <button onClick={handleLogout}>Logout</button>
-
     </div>
-  )
+  );
 }
 
-export default AddServer
+export default AddServer;
